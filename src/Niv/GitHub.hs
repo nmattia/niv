a1fad1c705ea0d6bd39bdaa6106f60db19e7539c
Nicolas Mattia <nicolas@nmattia.com>
2020-03-30 20:51:10 +0200

    Consider .tgz as .tar.gz

--- src/Niv/GitHub.hs
+++ src/Niv/GitHub.hs
@@ -33,7 +33,7 @@ githubUpdate prefetch latestRev ghRepo = proc () -> do
       (useOrSet "url_template" <<< completeSpec) <+> (load "url_template") -<
       ()
     url <- update "url" -< urlTemplate
-    let isTar = ("tar.gz" `T.isSuffixOf`) <$> url
+    let isTar = (\u -> "tar.gz" `T.isSuffixOf` u || ".tgz" `T.isSuffixOf` u) <$> url
     useOrSet "type" -< bool "file" "tarball" <$> isTar :: Box T.Text
     let doUnpack = isTar
     _sha256 <- update "sha256" <<< run (\(up, u) -> prefetch up u) -< (,) <$> doUnpack <*> url
