8b7317dd7a48ef6b6013cf2e6c074b76a5918044
Ben Sima <ben@bsima.me>
2020-05-01 08:49:44 -0700

    Add ormolu to dev environment
    Closes #221

--- nix/default.nix
+++ nix/default.nix
@@ -1,9 +1,14 @@
 { sources ? import ./sources.nix }:
+let
+  ormoluCompiler = "ghc865";
+in
 import sources.nixpkgs {
   overlays = [
     (_: pkgs: { inherit sources; })
     (_: pkgs: { nixpkgs-fmt = import pkgs.sources.nixpkgs-fmt {}; })
     (_: pkgs: { termtosvg = pkgs.callPackage ./termtosvg.nix {}; })
+    (import ./haskell-overlay.nix { inherit ormoluCompiler; })
+    (_: pkgs: { ormolu = pkgs.haskell.packages."${ormoluCompiler}".ormolu; })
   ];
   config = {};
 }
